64050f5299eb3d2046172762c148710953e6b248
fengxuanll <fengxuanll@qq.com>
2016-11-22 15:32:57 +0800

    增加公共方法

--- Core/Anchi.ERP.Common/Configuration/AppSettingReader.cs
+++ Core/Anchi.ERP.Common/Configuration/AppSettingReader.cs
@@ -4,12 +4,15 @@ using System.Configuration;
 
 namespace Anchi.ERP.Common.Configuration
 {
+    /// <summary>
+    /// AppSetting配置读取器
+    /// </summary>
     public class AppSettingReader
     {
-        private static IDictionary<string, object> Dict;
+        private static IDictionary<string, string> Dict;
         static AppSettingReader()
         {
-            Dict = new Dictionary<string, object>();
+            Dict = new Dictionary<string, string>();
         }
 
         #region 获取字符串配置
@@ -23,10 +26,91 @@ namespace Anchi.ERP.Common.Configuration
             if (string.IsNullOrWhiteSpace(key))
                 return string.Empty;
 
-            if (!Dict.ContainsKey(key))
-                Dict[key] = ConfigurationManager.AppSettings[key];
+            string value;
+            Dict.TryGetValue(key, out value);
 
-            return Dict[key].ExtToString();
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                value = ConfigurationManager.AppSettings[key];
+                Dict[key] = value;
+            }
+
+            return value;
+        }
+        #endregion
+
+        #region 获取数字类型配置
+        /// <summary>
+        /// 获取数字类型配置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static int GetInt32(string key, int defaultValue = 0)
+        {
+            var value = GetString(key);
+
+            int result;
+            if (!int.TryParse(value, out result))
+                result = defaultValue;
+
+            return result;
+        }
+        #endregion
+
+        #region 获取浮点类型配置
+        /// <summary>
+        /// 获取浮点类型配置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static float GetFloat(string key, float defaultValue = 0)
+        {
+            var value = GetString(key);
+
+            float result;
+            if (!float.TryParse(value, out result))
+                result = defaultValue;
+
+            return result;
+        }
+        #endregion
+
+        #region 获取Boolean类型配置
+        /// <summary>
+        /// 获取Boolean类型配置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="defaultValue"></param>
+        /// <returns></returns>
+        public static bool GetBoolean(string key, bool defaultValue = false)
+        {
+            var value = GetString(key);
+
+            bool result;
+            if (!bool.TryParse(value, out result))
+                result = defaultValue;
+
+            return result;
+        }
+        #endregion
+
+        #region 获取字符串集合
+        /// <summary>
+        /// 获取字符串集合
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="splitChar"></param>
+        /// <param name="removeEmpty"></param>
+        /// <returns></returns>
+        public static IList<string> GetStringList(string key, string splitChar = ",", bool removeEmpty = true)
+        {
+            var value = GetString(key);
+            if (string.IsNullOrWhiteSpace(value))
+                return new List<string>();
+
+            return value.ToList(splitChar, removeEmpty);
         }
         #endregion
     }
